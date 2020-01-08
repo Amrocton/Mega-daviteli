a1f3b0f61448a07d44c1c72a33b54cacce4c9b5d
rovch <rovcharov@gmail.com>
2020-01-08 20:36:19 +0300

    Img added game_loop in development

--- main.py
+++ main.py
@@ -4,6 +4,36 @@ import random
 import sys
 
 pygame.init()
+screen = pygame.display.set_mode((800, 500))
+clock = pygame.time.Clock()
+levelnum = random.randint(1, 4)
+levelnum = 1
+back_surf = pygame.image.load(f'data/background{levelnum}.jpg')
+back_rect = back_surf.get_rect(center=(400, 250))
+screen.blit(back_surf, back_rect)
+
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, (0, 0, 0))
+    return textSurface, textSurface.get_rect()
+
+
+def button(msg, x, y, w, h, ic, ac, action=None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    print(click)
+    if x + w > mouse[0] > x and y + h > mouse[1] > y:
+        pygame.draw.rect(screen, ac, (x, y, w, h))
+
+        if click[0] == 1 and action != None:
+            action()
+    else:
+        pygame.draw.rect(screen, ic, (x, y, w, h))
+
+    smallText = pygame.font.SysFont("comicsansms", 20)
+    textSurf, textRect = text_objects(msg, smallText)
+    textRect.center = ((x + (w / 2)), (y + (h / 2)))
+    screen.blit(textSurf, textRect)
 
 
 def terminate():
@@ -42,10 +72,85 @@ class Tile(pygame.sprite.Sprite):
         self.rect = self.image.get_rect().move(tile_w * pos_x, tile_h * pos_y)
 
 
-class Player()
+class Player(pygame.sprite.Sprite):
+    def __init__(self, ):
+        super().__init__(player_group, all_sprites)
 
 
 all_sprites = pygame.sprite.Group()
 tiles_group = pygame.sprite.Group()
 land_group = pygame.sprite.Group()
-players_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+sg_red = pygame.sprite.Group()
+sg_green = pygame.sprite.Group()
+sg_blue = pygame.sprite.Group()
+sg_yellow = pygame.sprite.Group()
+
+
+def generate_level(level):
+    spawn_coords = []
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '.':
+                tiles_group.add(Tile('empty', x, y))
+            elif level[y][x] == '#':
+                tiles_group.add(Tile('land', x, y))
+                land_group.add(Tile('land', x, y))
+            elif level[y][x] == '@':
+                spawn_coords.append((tile_w * x, tile_h * y))
+    return spawn_coords
+
+
+FPS = 30
+
+
+def start_screen(screen):
+    intro_text = ["Мар10", "",
+                  "Правила игры:",
+                  "По газону МОЖНО ходить",
+                  "Коробки по два метра в высоту, навернёшься",
+                  "Прежде чем начать, введите название уровня в консоли!!!"]
+    back_surf = pygame.image.load(f'data/background{levelnum}.jpg')
+    back_rect = back_surf.get_rect(center=(400, 250))
+    screen.blit(back_surf, back_rect)
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('black'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+    unstart = True
+    while unstart:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                unstart = False
+        pygame.display.flip()
+        clock.tick(FPS)
+    game_start(screen)
+
+
+def game_start(screen):
+    lp = 3
+    while True:
+        screen.fill((0, 0, 0))
+        back_surf = pygame.image.load(f'data/background{levelnum}.jpg')
+        back_rect = back_surf.get_rect(center=(400, 250))
+        screen.blit(back_surf, back_rect)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+        pygame.display.flip()
+
+
+def game_loop(screen, playersnum, lp):
+    spawn_points = generate_level(load_level(levelnum))
+
+
+
+game_loop(screen, 2, 3)
