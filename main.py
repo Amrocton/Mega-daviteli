0603f762ce35848db67de1e43c05572f05429964
Roman <rovcharov@gmail.com>
2020-01-27 16:33:45 +0300

    Animetion will be later...

--- main.py
+++ main.py
@@ -2,7 +2,184 @@ import pygame
 import os
 import random
 import sys
-from GIFImage import GIFImage
+from pygame.locals import *
+from PIL import Image
+
+import time
+
+
+class GIFImage(object):
+    def __init__(self, filename):
+        self.filename = filename
+        self.image = Image.open(filename)
+        self.frames = []
+        self.get_frames()
+
+        self.cur = 0
+        self.ptime = time.time()
+
+        self.running = True
+        self.breakpoint = len(self.frames) - 1
+        self.startpoint = 0
+        self.reversed = False
+
+    def get_rect(self):
+        return pygame.rect.Rect((0, 0), self.image.size)
+
+    def get_frames(self):
+        image = self.image
+
+        pal = image.getpalette()
+        base_palette = []
+        for i in range(0, len(pal), 3):
+            rgb = pal[i:i + 3]
+            base_palette.append(rgb)
+
+        all_tiles = []
+        try:
+            while 1:
+                if not image.tile:
+                    image.seek(0)
+                if image.tile:
+                    all_tiles.append(image.tile[0][3][0])
+                image.seek(image.tell() + 1)
+        except EOFError:
+            image.seek(0)
+
+        all_tiles = tuple(set(all_tiles))
+
+        try:
+            while 1:
+                try:
+                    duration = image.info["duration"]
+                except:
+                    duration = 100
+
+                duration *= .001  # convert to milliseconds!
+                cons = False
+
+                x0, y0, x1, y1 = (0, 0) + image.size
+                if image.tile:
+                    tile = image.tile
+                else:
+                    image.seek(0)
+                    tile = image.tile
+                if len(tile) > 0:
+                    x0, y0, x1, y1 = tile[0][1]
+
+                if all_tiles:
+                    if all_tiles in ((6,), (7,)):
+                        cons = True
+                        pal = image.getpalette()
+                        palette = []
+                        for i in range(0, len(pal), 3):
+                            rgb = pal[i:i + 3]
+                            palette.append(rgb)
+                    elif all_tiles in ((7, 8), (8, 7)):
+                        pal = image.getpalette()
+                        palette = []
+                        for i in range(0, len(pal), 3):
+                            rgb = pal[i:i + 3]
+                            palette.append(rgb)
+                    else:
+                        palette = base_palette
+                else:
+                    palette = base_palette
+
+                pi = pygame.image.fromstring(image.tobytes(), image.size, image.mode)
+                pi.set_palette(palette)
+                if "transparency" in image.info:
+                    pi.set_colorkey(image.info["transparency"])
+                pi2 = pygame.Surface(image.size, SRCALPHA)
+                if cons:
+                    for i in self.frames:
+                        pi2.blit(i[0], (0, 0))
+                pi2.blit(pi, (x0, y0), (x0, y0, x1 - x0, y1 - y0))
+
+                self.frames.append([pi2, duration])
+                image.seek(image.tell() + 1)
+        except EOFError:
+            pass
+
+    def render(self, screen, pos):
+        if self.running:
+            if time.time() - self.ptime > self.frames[self.cur][1]:
+                if self.reversed:
+                    self.cur -= 1
+                    if self.cur < self.startpoint:
+                        self.cur = self.breakpoint
+                else:
+                    self.cur += 1
+                    if self.cur > self.breakpoint:
+                        self.cur = self.startpoint
+
+                self.ptime = time.time()
+
+        screen.blit(self.frames[self.cur][0], pos)
+
+    def seek(self, num):
+        self.cur = num
+        if self.cur < 0:
+            self.cur = 0
+        if self.cur >= len(self.frames):
+            self.cur = len(self.frames) - 1
+
+    def set_bounds(self, start, end):
+        if start < 0:
+            start = 0
+        if start >= len(self.frames):
+            start = len(self.frames) - 1
+        if end < 0:
+            end = 0
+        if end >= len(self.frames):
+            end = len(self.frames) - 1
+        if end < start:
+            end = start
+        self.startpoint = start
+        self.breakpoint = end
+
+    def pause(self):
+        self.running = False
+
+    def play(self):
+        self.running = True
+
+    def rewind(self):
+        self.seek(0)
+
+    def fastforward(self):
+        self.seek(self.length() - 1)
+
+    def get_height(self):
+        return self.image.size[1]
+
+    def get_width(self):
+        return self.image.size[0]
+
+    def get_size(self):
+        return self.image.size
+
+    def length(self):
+        return len(self.frames)
+
+    def reverse(self):
+        self.reversed = not self.reversed
+
+    def reset(self):
+        self.cur = 0
+        self.ptime = time.time()
+        self.reversed = False
+
+    def copy(self):
+        new = GIFImage(self.filename)
+        new.running = self.running
+        new.breakpoint = self.breakpoint
+        new.startpoint = self.startpoint
+        new.cur = self.cur
+        new.ptime = self.ptime
+        new.reversed = self.reversed
+        return new
+
 
 gnomes = []
 is_start = False
@@ -10,8 +187,6 @@ paused = False
 lp = 3
 GRAVITY = 1
 playersnum = 2
-m_vol = 50
-s_vol = 50
 pygame.init()
 screen = pygame.display.set_mode((800, 500), pygame.FULLSCREEN)
 clock = pygame.time.Clock()
@@ -35,11 +210,6 @@ press_sound = pygame.mixer.Sound('data/sound/Press.wav')
 def starting():
     global is_start
     is_start = True
-
-
-'''def volume_update():
-    pygame.mixer.music.set_volume(m_vol)
-    pygame.mixer.Sound.set_volume(s_vol)'''
 
 
 def new_game():
@@ -364,7 +534,6 @@ def generate_level(level):
 
 
 FPS = 60
-'''volume_update()'''
 
 
 def start_screen(screen):
@@ -376,7 +545,10 @@ def start_screen(screen):
                   "Прыгай противнику на голову!",
                   "Но будь осторожен:",
                   "Тебя могут втоптать тоже!!!",
-                  'Последний игрок победил!!!']
+                  'Последний игрок победил!!!',
+                  '',
+                  '',
+                  'Нажмите любую кнопку, чтобы продолжить']
     screen.fill(pygame.Color('darkseagreen'))
     font = pygame.font.Font(None, 30)
     text_coord = 50
@@ -425,10 +597,6 @@ def game_start(screen):
         button(f'Lifes:{lp}', 420, 200, 80, 40, (245, 245, 220), (145, 145, 120), lifes_change_drop)
         button('-', 370, 200, 40, 40, (245, 245, 220), (145, 145, 120), lifes_change_decrease)
         button('+', 510, 200, 40, 40, (245, 245, 220), (145, 145, 120), lifes_change_add)
-        button(f'Music:{m_vol}', 620, 100, 80, 40, (245, 245, 220), (145, 145, 120),
-               lifes_change_drop)
-        button('-', 570, 100, 40, 40, (245, 245, 220), (145, 145, 120), lifes_change_decrease)
-        button('+', 710, 100, 40, 40, (245, 245, 220), (145, 145, 120), lifes_change_add)
         button('Quit to intro', 300, 300, 150, 50, (245, 245, 220), (145, 145, 120), to_intro)
         button('Quit the game', 300, 400, 150, 50, (245, 245, 220), (145, 145, 120), terminate)
         button('Secret', 780, 480, 20, 20, (245, 245, 220), (145, 145, 120), secret)
@@ -437,11 +605,18 @@ def game_start(screen):
 
 
 def game_loop(screen):
+    gnome_lifes_table_move = {
+        'red': (100, 450),
+        'green': (300, 450),
+        'blue': (500, 450),
+        'yellow': (700, 450)
+    }
     global paused, lp, playersnum
     game_end = False
     pygame.mouse.set_pos([0, 0])
     levelnum = random.randint(1, 4)
     paused = False
+    smallText = pygame.font.Font('data/18676.ttf', 30)
     pygame.mixer.music.load('data/sound/Kirby.mp3')
     pygame.mixer.music.play(-1)
     clock = pygame.time.Clock()
@@ -545,10 +720,17 @@ def game_loop(screen):
                 if yellow_gnome:
                     if event.key == yellow_gnome.move_key[2]:
                         yellow_gnome.move_right = False
+        tiles_group.draw(screen)
+        player_group.draw(screen)
         if not paused and not game_end:
             for gnome in gnomes:
                 gnome.move(gnomes)
                 gnome.on_land()
+                textRed, textRedRect = text_objects(f'{gnome.color.upper()}:{gnome.lifes}', smallText)
+                textRedRect.center = gnome_lifes_table_move[gnome.color]
+                textRed = smallText.render(f'{gnome.color.upper()}:{gnome.lifes}', True,
+                                           (0, 0, 0))
+                screen.blit(textRed, textRedRect)
                 if gnome.dead:
                     gnomes.remove(gnome)
                     player_group.remove(gnome)
@@ -558,8 +740,6 @@ def game_loop(screen):
                         gnome.jumping = True
                         gnome.velocity_index = 10
                         break
-        tiles_group.draw(screen)
-        player_group.draw(screen)
         if paused and not game_end:
             screen.blit(dim_screen, (0, 0))
             button('Continue', 330, 100, 120, 50, (245, 245, 220), (145, 145, 120), unpause)
@@ -573,7 +753,6 @@ def game_loop(screen):
             pygame.mixer.music.play()
         if game_end:
             screen.blit(dim_screen, (0, 0))
-            smallText = pygame.font.Font('data/18676.ttf', 30)
             textSurf, textRect = text_objects(f'{gnomes[0].color.upper()} WINS!', smallText)
             textRect.center = (400), (60)
             textSurf = smallText.render(f'{gnomes[0].color.upper()} WINS!', True,
--- test.py
+++
@@ -1,8 +0,0 @@
-velocity = [-18.5, -17, -16.5, -15.5, -15, -14.5, -14, -13.5, -13, -12.5, -12, -11.5, -11, -10.5, 0, 10.5, 11, 11.5, 12, 12.5, 13, 13.5, 14, 14.5, 15, 15.5, 16.5, 17, 18.5]
-num = 2
-for i, i_num in enumerate(velocity):
-    if i_num < 0:
-        velocity[i] = i_num - num
-    elif i_num > 0:
-        velocity[i] = i_num + num
-print(velocity)
